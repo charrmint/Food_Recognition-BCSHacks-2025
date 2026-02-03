437613f4653bade1dc544ec3d3d119499c0196a5
charrmint <rbcalawagan.ca@gmail.com>
2026-02-02 21:55:04 -0800

    fe cleanup phase 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
-import { Box } from '@chakra-ui/react';
+
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import CreatePage from './features/CreatePage';
-import AskPage from './features/AskPage';
-import HomePage from './features/HomePage';
-import SignupPage from './features/feature-sets/SignUp/presenter/SignupPagePresenter';
-import RemovePage from './features/RemovePage';
-import Navbar from './components/Navbar';
-import LogIn from './features/LogIn';
-import { useFoodStore } from './store/FoodStore/food';
-import { useColorModeValue } from '@chakra-ui/react';
 import AuthPagePresenter from './features/feature-sets/Auth/presenter/AuthPagePresenter';
-import OAuthCallback from './features/feature-sets/Auth/presenter/OAuthCallbackPresenter';
 import OAuthCallbackPresenter from './features/feature-sets/Auth/presenter/OAuthCallbackPresenter';
 import { useAuthStore } from './store/AuthStore';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -38,30 +28,6 @@ function App() {
           <DashboardPresenter />
         </ProtectedRoute>
       } />
-
-      {/* <Route path= "/" element={
-        <ProtectedRoute>
-          <HomePage />
-        </ProtectedRoute>
-      } />
-
-      <Route path= "/addFood" element={
-        <ProtectedRoute>
-          <CreatePage />
-        </ProtectedRoute>
-      } />
-
-      <Route path= "/AskPage" element={
-        <ProtectedRoute>
-          <AskPage />
-        </ProtectedRoute>
-      } />
-
-      <Route path= "/removeFood" element={
-        <ProtectedRoute>
-          <RemovePage />
-        </ProtectedRoute>
-      } /> */}
     </Routes>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-//import { Provider } from "@/components/ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from './App.jsx';
